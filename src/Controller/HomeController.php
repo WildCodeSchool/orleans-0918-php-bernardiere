804f7a43c0c2058ac72f6cd544def0f922b70881
Gallier Guillaume <guillaume.gallier@sfr.fr>
2018-10-18 17:22:11 +0200

    ajout des trois vignettes sur la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,22 @@
 
 namespace Controller;
 
+use Model\HomeManager;
 
 class HomeController extends AbstractController
 {
+    /**
+     * @param int $id
+     * @return string
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
     public function homeAction()
     {
-        return $this->twig->render('home.html.twig');
+        $homeManager = new HomeManager($this->getPdo());
+        $products = $homeManager->selectRandomProduct();
+
+        return $this->twig->render('home.html.twig', ['products'=>$products]);
     }
 }
