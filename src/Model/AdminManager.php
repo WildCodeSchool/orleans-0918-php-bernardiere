1d758b32f94a7a042e11309f8f8780a00e1852b6
Gallier Guillaume <guillaume.gallier@sfr.fr>
2018-10-26 10:18:09 +0200

    modification du AdminManager

--- src/Model/AdminManager.php
+++ src/Model/AdminManager.php
@@ -29,10 +29,9 @@ class AdminManager extends AbstractManager
      */
     public function __construct(\PDO $pdo)
     {
-        parent::__construct(self::TABLE,$pdo);
+        parent::__construct(self::TABLE, $pdo);
     }
 
-<<<<<<< HEAD
     public function insert(Product $product): int
     {
         // prepared request
@@ -45,52 +44,6 @@ class AdminManager extends AbstractManager
         $statement->bindValue('image', $product->getImage(), \PDO::PARAM_STR);
         $statement->bindValue('description_produit', $product->getDescriptionProduct(), \PDO::PARAM_STR);
         $statement->bindValue('category_id', $product->getCategoryId(), \PDO::PARAM_INT);
-
-
-=======
-    /**
-     * @param \Model\Product $product
-     * @return int
-     */
-    public function insert(Product $product): int
-    {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`) VALUES (:name)");
-        $statement->bindValue('name', $product->getName(), \PDO::PARAM_STR);
->>>>>>> 082c8d4f1d1558794930023f72dfb55019a22aa8
-        if ($statement->execute()) {
-            return $this->pdo->lastInsertId();
-        }
     }
 }
 
-<<<<<<< HEAD
-=======
-    /**
-     * @param int $id
-     */
-    public function delete(int $id): void
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-    }
-
-
-    /**
-     * @param Item $item
-     * @return int
-     */
-    public function update(Item $item):int
-    {
-
-        // prepared request
-        $statement = $this->pdo->prepare("UPDATE $this->table SET `title` = :title WHERE id=:id");
-        $statement->bindValue('id', $item->getId(), \PDO::PARAM_INT);
-        $statement->bindValue('title', $item->getTitle(), \PDO::PARAM_STR);
-
-
-        return $statement->execute();
-    }
-}
->>>>>>> 082c8d4f1d1558794930023f72dfb55019a22aa8
