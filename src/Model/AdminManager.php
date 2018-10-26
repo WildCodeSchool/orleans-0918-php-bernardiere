eea3c9c33f86aeaba5d531d1cc22ab19c40e8c81
Gallier Guillaume <guillaume.gallier@sfr.fr>
2018-10-26 09:42:37 +0200

    modification des fichiers suite aux commentaires

---
+++ src/Model/AdminManager.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder4
+ * Date: 26/10/18
+ * Time: 09:38
+ */
+
+namespace Model;
+
+
+class AdminManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'product';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct(\PDO $pdo)
+    {
+        parent::__construct(self::TABLE, $pdo);
+    }
+
+    public function insert(Product $product): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare('INSERT INTO ' . $this->table . ' 
+            (`category_id`,`name`,`product_begin`,`product_end`,`image`,`description_produit`) 
+            VALUES (:category_id,:name_product,:product_begin,:product_end,:image,:description_produit) ');
+        $statement->bindValue('name_product', $product->getName(), \PDO::PARAM_STR);
+        $statement->bindValue('product_begin', $product->getProductBegin(), \PDO::PARAM_INT);
+        $statement->bindValue('product_end', $product->getProductEnd(), \PDO::PARAM_INT);
+        $statement->bindValue('image', $product->getImage(), \PDO::PARAM_STR);
+        $statement->bindValue('description_produit', $product->getDescriptionProduct(), \PDO::PARAM_STR);
+        $statement->bindValue('category_id', $product->getCategoryId(), \PDO::PARAM_INT);
+
+
+        if ($statement->execute()) {
+            return $this->pdo->lastInsertId();
+        }
+    }
+}
+
